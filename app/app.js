413da5bd5f07e12abd148837d8a2073cfdb74b3e
rams99 <aroldrams@gmail.com>
2020-11-12 21:21:07 +0000

    order test pipeline v2

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const bodyParser = require('body-parser');
-const port = 4545;
+const port =process.env.port || 4545;
 const app = express();
 const router = require('./route/route');
 
