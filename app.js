bd1ec6dc6d61130f93968c63745fcbe9480607ce
rams99 <aroldrams@gmail.com>
2020-12-03 15:36:23 +0000

    cors origine fixed

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 const port = process.env.port || 4545;
 const app = express();
 const router = require('./app/route/route');
 const adminRouter = require('./app/route/admin-route');
 const deliveryRouter = require('./app/route/delivery-route');
 const restaurantRouter = require('./app/route/restaurant-route');
-
+const frontend = 'http://localhost:4200'
 app.use(express.json());
+app.use(cors({origin: frontend}));
 app.use('/api', router);
 app.use('/api/admin', adminRouter);
 app.use('/api/resto', restaurantRouter);
